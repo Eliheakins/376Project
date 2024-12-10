0490b6c761be29f8664eec8bf8b2c3bbdb723dab
annarlieb <120057684+annarlieb@users.noreply.github.com>
2024-12-09 23:13:14 -0500

    organized variables and overall function

--- GpsSimMoveXCopy2.m
+++ GpsSimMoveXCopy2.m
@@ -1,11 +1,20 @@
-clear all; clc; close all; 
+% static variables (these dont change)
 signalspeed = 299792.458;
-numSatelites = 6; 
-
 satZ = 26570;
+surfaceZ = 6370;
+
+% independent variables (play around with these
+numSatelites = 4; 
+numMoves = 50;
+stepdist = 0.1; 
+noisefactor = 1e-10; 
+
+% set up stuff
 pos = [];
 phi = pi/6;
-theta = linspace(0, 2*pi, numSatelites + 1); % Adjusted for numSatelites
+theta = linspace(0, 2*pi, numSatelites + 1);  
+xAll = zeros(4, numMoves);
+error = zeros(1, numMoves);
  
 % Position calculation loop
 for i = 1:numSatelites
@@ -16,53 +25,47 @@ for i = 1:numSatelites
     ];
 end
 
-surfaceZ = 6370;
-numMoves = 50;
-xAll = zeros(4, numMoves);
-error = zeros(1, numMoves);
-stepdist = 0.1; 
-noisefactor = 1e-10; % Adjust as needed to simulate noise magnitude
 
 for i = 1:numMoves
+
+    % calculate time taken from each satelite to the phone
+
     initialPosActual = [(i-1) * stepdist, 0, surfaceZ];
-    distance = zeros(numSatelites,1);
     randnoise = zeros(numSatelites,1);
+    distance = zeros(numSatelites,1);
+
     for j = 1:numSatelites
         distance(j) = norm(pos(j, :) - initialPosActual);
         randnoise(j) = rand()*noisefactor*i; % sizes by a factor of i
     end
     t = distance ./ signalspeed + randnoise;
 
-
     % Calculate PHONE position using TIMES
-   
-    x0 = [0; 0; 6370; 0];
-    % A = x positions of sats; B = y positions of sats; C = z positions of sats
-    A = pos(:, 1); B = pos(:, 2); C = pos(:, 3);
-
     syms x y z d
+    x0 = [0; 0; 6370; 0];
+    A = pos(:, 1); B = pos(:, 2); C = pos(:, 3); % A = x positions of sats; B = y positions of sats; C = z positions of sats
 
     fSym = sym([]); % Initialize fSym as a column vector
     for j = 1:numSatelites 
+        % this is the function we are trying to minimize for each satelite
         fSym(j) = (x - A(j))^2 + (y - B(j))^2 + (z - C(j))^2 - (signalspeed * (t(j) - d))^2;
     end
     fSym = fSym';
-
     JSym = jacobian(fSym, [x, y, z, d]);
 
     f_num = matlabFunction(fSym, 'Vars', [x, y, z, d]);
     J_num = matlabFunction(JSym, 'Vars', [x, y, z, d]);
-
     f_wrapper = @(v) f_num(v(1), v(2), v(3), v(4));
     J_wrapper = @(v) J_num(v(1), v(2), v(3), v(4));
 
+    x = newtons_method_n2(f_wrapper, J_wrapper, x0); % numerical solution!
+    error(1,i) = norm(x(1:3)' - initialPosActual);
+    xAll(:, i) = x;
 
-    x = newtons_method_n2(f_wrapper, J_wrapper, x0);
     if mod(i, 10) == 0
         disp(i)
     end
-    error(1,i) = norm(x(1:3)' - initialPosActual);
-    xAll(:, i) = x;
+
 end
 
-plot(1:numMoves, error)
+plot(1:numMoves, error,'r')
