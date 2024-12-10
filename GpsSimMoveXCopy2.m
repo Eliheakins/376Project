2f4d049a047a785b919205d567fceb5c35ca3afb
annarlieb <120057684+annarlieb@users.noreply.github.com>
2024-12-09 19:50:05 -0500

    Update how we do noise

--- GpsSimMoveXCopy2.m
+++ GpsSimMoveXCopy2.m
@@ -1,6 +1,6 @@
 clear all; clc; close all; 
 signalspeed = 299792.458;
-numSatelites = 6; % Variable for the number of satellites
+numSatelites = 6; 
 
 satZ = 26570;
 pos = [];
@@ -17,32 +17,33 @@ for i = 1:numSatelites
 end
 
 surfaceZ = 6370;
-numMoves = 100;
+numMoves = 50;
 xAll = zeros(4, numMoves);
 error = zeros(1, numMoves);
 stepdist = 0.1; 
+noisefactor = 1e-10; % Adjust as needed to simulate noise magnitude
 
 for i = 1:numMoves
     initialPosActual = [(i-1) * stepdist, 0, surfaceZ];
     distance = zeros(numSatelites,1);
+    randnoise = zeros(numSatelites,1);
     for j = 1:numSatelites
         distance(j) = norm(pos(j, :) - initialPosActual);
+        randnoise(j) = rand()*noisefactor*i; % sizes by a factor of i
     end
-    t = distance ./ signalspeed;
-    noise_level = (1e-11) * i; % Adjust as needed to simulate noise magnitude
-    t = t + noise_level * randn(size(t)); 
+    t = distance ./ signalspeed + randnoise;
 
-    % Calculate position
+
+    % Calculate PHONE position using TIMES
+   
     x0 = [0; 0; 6370; 0];
-    % A = x positions of all the sats
-    % B = y positions of all the sats
-    % C = z positions of all the sats
+    % A = x positions of sats; B = y positions of sats; C = z positions of sats
     A = pos(:, 1); B = pos(:, 2); C = pos(:, 3);
 
     syms x y z d
 
     fSym = sym([]); % Initialize fSym as a column vector
-    for j = 1:numSatelites % CHANGED
+    for j = 1:numSatelites 
         fSym(j) = (x - A(j))^2 + (y - B(j))^2 + (z - C(j))^2 - (signalspeed * (t(j) - d))^2;
     end
     fSym = fSym';
