f24982b337b27d7ed6bbec167a48ede6f1292a0e
Eli <eheakins@colgate.edu>
2024-12-11 10:20:14 -0500

    Added start end to plot

--- plotGPS.m
+++ plotGPS.m
@@ -4,23 +4,32 @@ function [] = plotGPS(estPos, actualPos, satPos)
 
     % Create a sphere for Earth
     [x, y, z] = sphere(50);
-    surf(x * earth_radius, y * earth_radius, z * earth_radius, 'FaceColor', 'black', 'EdgeColor', 'none');
+    h_earth = surf(x * earth_radius, y * earth_radius, z * earth_radius, ...
+                   'FaceColor', 'black', 'EdgeColor', 'none');
     hold on;
     
-    % Plot satellite positions (no labels)
-    scatter3(satPos(:, 1), satPos(:, 2), satPos(:, 3), 100, 'red', 'filled');
+    % Plot satellite positions
+    h_sat = scatter3(satPos(:, 1), satPos(:, 2), satPos(:, 3), 100, 'red', 'filled');
     
-    % Plot actual positions of the target (no labels)
-    scatter3(actualPos(1, :), actualPos(2, :), actualPos(3, :), 50, 'green', 'filled');
+    % Plot the path and positions of the actual target
+    h_actual_path = plot3(actualPos(1, :), actualPos(2, :), actualPos(3, :), ...
+                          'g-', 'LineWidth', 2);
+    h_actual_start = scatter3(actualPos(1, 1), actualPos(2, 1), actualPos(3, 1), ...
+                              200, 'green', 'o', 'filled');
+    h_actual_end = scatter3(actualPos(1, end), actualPos(2, end), actualPos(3, end), ...
+                            200, 'yellow', 'o', 'filled');
     
-    % Plot the path of the actual target (a line connecting the actual positions)
-    plot3(actualPos(1, :), actualPos(2, :), actualPos(3, :), 'g-', 'LineWidth', 2);
+    % Plot the path and positions of the estimated target
+    h_est_path = plot3(estPos(1, :), estPos(2, :), estPos(3, :), 'b-', 'LineWidth', 2);
+    h_est_start = scatter3(estPos(1, 1), estPos(2, 1), estPos(3, 1), 200, 'blue', '^', 'filled');
+    h_est_end = scatter3(estPos(1, end), estPos(2, end), estPos(3, end), 200, 'cyan', '^', 'filled');
 
-    % Plot estimated positions of the target (no labels)
-    scatter3(estPos(1, :), estPos(2, :), estPos(3, :), 50, 'blue', 'filled');
-    
-    % Plot the path of the estimated target (a line connecting the estimated positions)
-    plot3(estPos(1, :), estPos(2, :), estPos(3, :), 'b-', 'LineWidth', 2);
+    % Corrected legend order
+    legend([h_earth, h_sat, h_actual_path, h_actual_start, h_actual_end, ...
+            h_est_path, h_est_start, h_est_end], ...
+           {'Earth', 'Satellites', 'Actual Path', 'Actual Start', 'Actual End', ...
+            'Estimated Path', 'Est Start', 'Est End'}, ...
+           'Location', 'northeastoutside');
 
     % Plot settings
     title('Satellite and Target Positions Above Earth');
